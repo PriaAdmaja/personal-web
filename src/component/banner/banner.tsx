f8e2c1615d06794f20562b9f4de8a61d8dc1082e
Pria Admaja <pria.admaja@havedev.com>
2024-07-04 22:44:30 +0700

    add profile pict in banner

--- src/component/banner/banner.tsx
+++ src/component/banner/banner.tsx
@@ -1,7 +1,13 @@
 import "./banner.css";
+import profilePict from '../../assets/profile.webp'
 
 const Banner = () => {
-  return <section className="banner-container">Banner</section>;
+  return (
+    <section className="banner-container">
+      <p className="intro">Hey There,<br/>I'm Pria </p>
+      <img src={profilePict} alt="profile" className="profile"/>
+    </section>
+  );
 };
 
 export default Banner;
