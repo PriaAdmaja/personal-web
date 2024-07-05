ad6c38e274c9aab257bdcd9dcf94bee6bf476cd3
Pria Admaja <pria.admaja@havedev.com>
2024-07-05 16:18:09 +0700

    add brush background and change favicon

--- src/component/banner/banner.tsx
+++ src/component/banner/banner.tsx
@@ -1,11 +1,23 @@
 import "./banner.css";
-import profilePict from '../../assets/profile.webp'
+import profilePict from "../../assets/profile.webp";
+import brush from "../../assets/brush.webp";
 
 const Banner = () => {
   return (
     <section className="banner-container">
-      <p className="intro">Hey There,<br/>I'm Pria </p>
-      <img src={profilePict} alt="profile" className="profile"/>
+      <img src={brush} alt="brush" className="brush" />
+      <img src={profilePict} alt="profile" className="profile" />
+      <div className="greeting">
+        <p className="intro">
+          Hey There,
+          <br />
+          I'm Pria
+        </p>
+        <p className="first-introduce">
+          Web Developer, <br />
+          with more than 1 year experience
+        </p>
+      </div>
     </section>
   );
 };
